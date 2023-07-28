0fc1537b0a6c8afddfa582c56f7b00c5f7b614b9
Devpriya Shivani <dpshiv12@gmail.com>
2023-07-28 09:44:37 +0530

    wip

--- js/watchlist.js
+++ js/watchlist.js
@@ -1,29 +1,47 @@
 window.onload = function() {
   document.querySelector('.pre-search-2').innerHTML = ''
   let value = JSON.parse(localStorage.getItem('item'));
-  for (let data of value) {
-    document.querySelector('.main-content').innerHTML += `
-      <div class="post-search" id="${data.imdbID}">
-      <img src="${data.Poster}"> 
-      <div class="info">
-        <div class="main-info">
-          <h1> ${data.Title} </h1>
-          <h4> ⭐ ${data.imdbRating} </h4>
-        </div>
-        <div class="detailed-info">
-          <p> ${data.Runtime} </p>
-          <p> ${data.Genre} </p>
-          <div>
-            <button class="remove-from-watchlist"> 
-              <i class="fa-solid fa-minus" data-watchlist=${data.imdbID}></i>
-            </button> Remove
+  if (value.length) {
+    for (let data of value) {
+      document.querySelector('.main-content').innerHTML += `
+        <div class="post-search" id="${data.imdbID}">
+        <img src="${data.Poster}"> 
+        <div class="info">
+          <div class="main-info">
+            <h1> ${data.Title} </h1>
+            <h4> ⭐ ${data.imdbRating} </h4>
           </div>
+          <div class="detailed-info">
+            <p> ${data.Runtime} </p>
+            <p> ${data.Genre} </p>
+            <div>
+              <button class="remove-from-watchlist"> 
+                <i class="fa-solid fa-minus" data-watchlist=${data.imdbID}></i>
+              </button> Remove
+            </div>
+          </div>
+          <p class="plot"> ${data.Plot} </p>
         </div>
-        <p class="plot"> ${data.Plot} </p>
       </div>
-    </div>
-    <hr>
-  `
+      <hr>
+    `
+    }
+  }
+  else {
+    document.querySelector('.main-content').innerHTML = `
+      <div class="pre-search-2">
+        <div class="initial-message">
+          <label> Your watchlist is looking a little empty... </label>
+        </div>
+        <div class="sub-title-watchlist">
+          <a href="index.html">
+            <button class="back-to-search"> 
+              <i class="fa-thin fa-plus"></i>
+            </button> Let’s add some movies!
+          </a>
+        </div>
+      </div>
+    `
   }
 }
 
